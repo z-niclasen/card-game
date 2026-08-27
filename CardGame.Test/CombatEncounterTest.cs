58e57513d756adeba6de2c056cfe9b1f1148c8b3
Zacharias Niclasen <zacharias@live.dk>
2026-08-27 13:32:57 +0200

    Added test project.

---
+++ CardGame.Test/CombatEncounterTest.cs
@@ -0,0 +1,16 @@
+﻿namespace CardGame.Test;
+
+public class CombatEncounterTest
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
