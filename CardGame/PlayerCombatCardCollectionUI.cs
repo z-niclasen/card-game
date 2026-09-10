f1f2737dc5afd9c1dd67a64503ac239ed5dcc918
Zacharias Niclasen <zacharias@live.dk>
2026-09-10 12:10:05 +0200

    Made visual changes to CombatCardCollectionUI.

--- CardGame/PlayerCombatCardCollectionUI.cs
+++ CardGame/PlayerCombatCardCollectionUI.cs
@@ -19,16 +19,30 @@ public partial class PlayerCombatCardCollectionUI : Node2D
 			AddObservers();
 		}
 	}
-	
 	private ICombatCardCollection _collection;
+
+	[Export]
+	private int HandOffset { get; set; } = 200;
+
+	[Export]
+	private int CardSeparator { get; set; } = 30;
 
 	private PackedScene _cardUIScene;
 
 	private readonly List<CardUI> _hand = [];
+	private readonly Dictionary<ICard, CardUI> _cardMap = new();
+	
+	private Button _discardPileButton;
+	private Button _drawPileButton;
+	private Control _handContainer;
 
 	public override void _Ready()
 	{
 		_cardUIScene = ResourceLoader.Load<PackedScene>("uid://byss7fge32s1p");
+		
+		_discardPileButton = GetNode<Button>("%DiscardPileButton");
+		_drawPileButton = GetNode<Button>("%DrawPileButton");
+		_handContainer = GetNode<Control>("%HandContainer");
 	}
 
 	private void CollectionOnOnDrawCard(ICombatCardCollection collectionMutable, ICard card)
@@ -38,7 +52,7 @@ public partial class PlayerCombatCardCollectionUI : Node2D
 
 	private void CollectionOnOnDiscardCard(ICombatCardCollection collectionMutable, ICard card)
 	{
-		throw new NotImplementedException();
+		DisposeCard(card);
 	}
 
 	private void InstantiateCard(ICard card)
@@ -48,37 +62,56 @@ public partial class PlayerCombatCardCollectionUI : Node2D
 		
 		cardUI.Card = card;
 		_hand.Add(cardUI);
+		_cardMap.Add(card, cardUI);
 		
 		DisplayHand();
+	}
+
+	private void DisposeCard(ICard card)
+	{
+		CardUI cardUI =  _cardMap[card];
+		
+		cardUI.QueueFree();
+		
+		_hand.Remove(cardUI);
+		_cardMap.Remove(card);
 	}
 	
 	private void DisplayHand()
 	{
-		if (_hand.Count == 0)
-			return;
-
-		float viewportWidth = GetViewport().GetVisibleRect().Size.X;
-		float viewportHeight = GetViewport().GetVisibleRect().Size.Y;
-		int midPoint = (int)(viewportWidth / 2);
-		//float handWidth = (int)(0.8 * viewportWidth);
-		
 		int handCount = _hand.Count;
-		int cardWidth = _hand[0].Width;
-		int cardHeight = _hand[0].Height;
-		int sep = (int)(0.2 * cardWidth);
+		
+		if (handCount == 0)
+			return;
+		
+		//float viewportWidth = GetViewport().GetVisibleRect().Size.X;
+		//float viewportHeight = GetViewport().GetVisibleRect().Size.Y;
+		
+		float totalHandWidth = _handContainer.Size.X;
+		float handHorizontalMidPoint = _handContainer.Position.X + totalHandWidth / 2;
+		float handVerticalMidPoint = _handContainer.Position.Y + _handContainer.Size.Y / 2;
 
-		int handWidth = handCount * cardWidth + (handCount - 1) * sep;
-		int startX = midPoint - handWidth / 2;
+		int cardHeight = _hand[0].Height;
+		int cardWidth = _hand[0].Width;
+		int totalCardWidths = cardWidth * handCount + CardSeparator * (handCount - 1);
+		
+		int actualCardWidth = (totalCardWidths <= totalHandWidth) ? totalCardWidths / handCount 
+			: (int)(totalHandWidth / handCount);
+		
+		bool evenCards = handCount % 2 == 0;
+
+		int startX = evenCards ? (int)(handHorizontalMidPoint - ((handCount / 2) * actualCardWidth) + (int)(actualCardWidth * 0.5f))
+			: (int)(handHorizontalMidPoint - ((float)handCount - 1) / 2 * actualCardWidth);
 
 		for (int i = 0; i < handCount; i++)
 		{
-			CardUI card =  _hand[i];
+			CardUI card = _hand[i];
 			card.Visible = true;
 
-			int newY = (int)(viewportHeight * 0.9 - (int)(cardHeight / 2));
-			int newX = startX + (int)(0.5 * cardWidth) + i * (sep + cardWidth);
-			
-			card.Position = Position with { X =  newX, Y = newY };
+			float newY = handVerticalMidPoint;
+			float newX = startX + i * actualCardWidth;
+
+			card.Position = Position with { X = newX, Y = newY };
 		}
 	}
 
